356b1b4a1effe2614b768fad936265b557d2377c
mmotahari <mmotahari6685@gmail.com>
2025-04-10 14:40:57 +0330

    14040121-swaptwonumberfunction

--- pointers/swapfunction.c
+++ pointers/swapfunction.c
@@ -9,7 +9,7 @@ int main(){
     int n1 , n2;
     scanf("%d" , &n1);
     scanf("%d" , &n2);
-    printf("%d , %d" n1,n2);
-    swap(n1,n2);
-    printf("%d , %d" n1,n2);
+    printf("%d , %d  \n", n1,n2);
+    swap(&n1,&n2);
+    printf("%d , %d", n1,n2);
 }
